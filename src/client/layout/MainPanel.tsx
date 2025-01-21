e80ebbe0875b06d5bd6a61b1528de59f406ebabd
Alem Tuzlak <t.zlak@hotmail.com>
2025-01-21 17:55:06 +0100

    added react-scan, made bippy a dependency

--- src/client/layout/MainPanel.tsx
+++ src/client/layout/MainPanel.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx"
 import { useState } from "react"
+import { useScan } from "react-scan"
 import { useDetachedWindowControls, useSettingsContext } from "../context/useRDTContext.js"
 import { useAttachWindowListener } from "../hooks/useAttachListener.js"
 import { useDebounce } from "../hooks/useDebounce.js"
@@ -24,10 +25,12 @@ const useResizeDetachedPanel = () => {
 const MainPanel = ({ children, isOpen, isEmbedded = false, className }: MainPanelProps) => {
 	const { settings } = useSettingsContext()
 	const { detachedWindow } = useDetachedWindowControls()
-	const { height, panelLocation } = settings
+	const { height, panelLocation, enableReactScan } = settings
 	const { enableResize, disableResize, isResizing } = useResize()
 	useResizeDetachedPanel()
-
+	useScan({
+		enabled: enableReactScan,
+	})
 	return (
 		<div
 			data-testid="react-router-devtools-main-panel"
